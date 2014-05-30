be1d58fdc559f824af48eb8ea07229957252d3e8
Bob McKee <bmckee@bywires.com>
2014-05-30 14:49:55 -0400

    update version number

--- chara/__init__.py
+++ chara/__init__.py
@@ -1,2 +1,4 @@
 from .chara import Spy, Call
 from .integration import record, replay
+
+__version__ = '0.1.3'
--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ keywords = [
     'test',
 ]
 
-version = '0.1.2'
+version = '0.1.3'
 
 setup(
     name='chara',
